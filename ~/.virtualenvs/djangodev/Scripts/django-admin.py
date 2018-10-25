2f962835f1c44731172e7afaaf82db419325bea0
Jeff Cai <jefcai@oradev.oraclecorp.com>
2018-10-24 17:21:55 -0700

    Fixed #99999 -- Added a shortcut function to make toast.

---
+++ ~/.virtualenvs/djangodev/Scripts/django-admin.py
@@ -0,0 +1,6 @@
+#!c:\django-clone\django\~\.virtualenvs\djangodev\scripts\python.exe
+# EASY-INSTALL-DEV-SCRIPT: 'Django==2.2.dev20181024004649','django-admin.py'
+__requires__ = 'Django==2.2.dev20181024004649'
+__import__('pkg_resources').require('Django==2.2.dev20181024004649')
+__file__ = 'C:\\django-clone\\django\\django\\bin\\django-admin.py'
+exec(compile(open(__file__).read(), __file__, 'exec'))
